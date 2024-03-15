dad0a721bd5a449e3b0b59073645f32c759e24d0
codewith-Faizi <160515803+codewith-Faizi@users.noreply.github.com>
2024-03-15 11:31:21 -0700

    Exercise 2/9
    Typescript Assignment

---
+++ main.js
@@ -0,0 +1,33 @@
+// Q2
+//personal Message
+var personName = "Faizan";
+console.log("Hello ".concat(personName, ", would you like to some python today"));
+// Q3
+//Name Cases
+var Name1 = "Faizan";
+console.log(Name1.toLowerCase());
+console.log(Name1.toUpperCase());
+console.log(Name1.charAt(0).toUpperCase() + Name1.slice(1).toLowerCase());
+// Q4
+//Famous Quot
+console.log('Albert Einstein once said,"A person who never made a mistake never tried anything new."');
+// Q5
+//Famous Quote 2
+var famous_person = "Albert Einstein";
+var message = "".concat(famous_person, " once said,\"A person who never made a mistake never tride anything new\"");
+console.log(message);
+//Q 6
+//Stripping Names
+var Name = "\t\n Faizan \t\n";
+console.log(Name);
+console.log(Name.trim());
+// Q7/8
+// Number Eight
+console.log(5 + 3);
+console.log(10 - 2);
+console.log(4 * 2);
+console.log(16 / 2);
+// Q9
+// Favorite Number
+var favoriteNumber = 8;
+console.log("my favorite number is ".concat(favoriteNumber, "."));
